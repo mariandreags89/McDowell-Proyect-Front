1d29f6cf59e0c80dd747aa2bf5477e3bd93aa25e
TanUve <tania.velasco.carvajal@gmail.com>
2023-01-22 00:32:22 +0100

    new components, css code and images

--- mcdowells-proyect/src/App.js
+++ mcdowells-proyect/src/App.js
@@ -2,12 +2,26 @@ import './assets/App.css'
 import React from 'react';
 import ClientsHome from "./components/clients/ClientsHome";
 import Menus from './components/clients/Menus';
+import Cart from './components/clients/Cart';
+import ProductDetails from './components/clients/ProductDetails';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return(
-  <>
-    <Menus />
-  </>
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<ClientsHome/>}/>
+      <Route path='/menus' element={<Menus/>}/>
+      {/*
+      En la ruta de los detalles del producto habría que meterle en la url el id dinámico y usar useParams después
+      en el componente que le corresponde, ya que esta página se va a ver en función del id del producto al que demos
+       */}
+      <Route path='/details' element={<ProductDetails/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
--- mcdowells-proyect/src/components/clients/Menus.js
+++ mcdowells-proyect/src/components/clients/Menus.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import mcTitle from '../../assets/images/title.png'
 import '../../assets/clients/menus.css'
+import { useNavigate } from 'react-router-dom';
 
 
 function Menus() {
+    const navigate = useNavigate();
     return (
         <>
             <div className="containerMenu">
@@ -12,24 +14,29 @@ function Menus() {
                     <div className='topMenu'>
                         <img className='mcTitle' src={mcTitle} alt='NOT FOUND' />
                     </div>
-                    <div className='centerLeft'>
-                        <img className='mcJr' src='https://cdn-icons-png.flaticon.com/512/1581/1581006.png' alt='NOT FOUND' />
-                        <button className='mcJrBtn'>
-                            MENÚ McDOWELL'S JR
-                            4,25€
-                        </button>
-                    </div>
-                    <div className='centerRight'>
-                        <img className='mcBig' src='https://cdn-icons-png.flaticon.com/512/1581/1581006.png' alt='NOT FOUND' />
-                        <button className='mcBtn'>
-                            MENÚ McDOWELL'S
-                            8,50€
-                        </button>
+                    <div className='centerMenu'>
+                        <div className='centerLeft'>
+                            <img className='mcJr' src='https://cdn-icons-png.flaticon.com/512/7451/7451012.png' alt='NOT FOUND' />
+                            {/*
+                            Hay que tener en cuenta que hay que meter el id dinámico en ambos botones
+                             */}
+                            <button className='mcJrBtn' onClick={() => navigate(`/details`)}>
+                                MENÚ McDOWELL'S JR
+                                4,25€
+                            </button>
+                        </div>
+                        <div className='centerRight'>
+                            <img className='mcBig' src='https://cdn-icons-png.flaticon.com/512/9425/9425772.png' alt='NOT FOUND' />
+                            <button className='mcBtn' onClick={() => navigate(`/details`)} >
+                                MENÚ McDOWELL'S
+                                8,50€
+                            </button>
 
+                        </div>
                     </div>
                     <div className='bottomMenu'>
                         <div className='bottomLeft' >
-                            <p className='resumen'>Ver resumen del pedido</p>
+                            <p className='resumen' onClick={ ()=>navigate(`/Cart`)}>Ver resumen del pedido</p>
                             <p className='total' >TOTAL: { }€</p>
                         </div>
                         <div className='bottomRight' >
