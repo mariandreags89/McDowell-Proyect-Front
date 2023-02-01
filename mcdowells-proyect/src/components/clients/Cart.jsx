59f40c04ada3dd5bd473d7769ca9f4b1bc7f7a1c
TanUve <tania.velasco.carvajal@gmail.com>
2023-02-01 21:15:34 +0100

    added code

--- mcdowells-proyect/src/components/clients/Cart.jsx
+++ mcdowells-proyect/src/components/clients/Cart.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import resumen from '../../assets/images/resumen.png'
 import pedido from '../../assets/images/resumen2.png'
 import '../../assets/clients/cart.css'
 import { useNavigate } from 'react-router-dom';
-//<img className='mcJr' src='https://cdn-icons-png.flaticon.com/512/1581/1581006.png' alt='NOT FOUND' />
+import { useCartContext } from '../../context/ShoppingCartContext';
+import { useEffect } from 'react';
+import axios from 'axios';
+
 
 
 
 function Cart() {
-
     const navigate = useNavigate();
+    const context = useCartContext();
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            const response = await axios.get(`http://localhost:3001/api/products/all-products`);
+            setProducts(response.data);
+        }
+        getProducts();
+    }, [])
+
+    const addProduct = () => {
+
+        const isInCart = context.cart.find(item => item.id_product === products.id_product)
+
+        if (isInCart) {
+            const setOneProd = context.cart.map(item =>  //En este punto, si coincide isInCart es lo mismo que product
+                item.id_product === isInCart.id_product ? {
+                    ...isInCart,
+                    quantity: isInCart.quantity + 1,
+                    total: isInCart.price * (isInCart.quantity + 1)
+                } : isInCart
+            );
+            context.setCart(setOneProd); //Estás limpiando el carrito que había previamente se actualiza con lo nuvo
+            console.log(setOneProd);
+        }
+
+        const setTotalPrice = context.totalCart.map((item) => {
+            return (
+                {
+                    totalPrice: item.totalPrice + products.price,
+                    totalQuantity: products.totalQuantity
+                })
+        })
+        context.setTotalCart(setTotalPrice)//actualizamos el total
+        console.log(setTotalPrice);
+
+    }
+
 
     return (
         <>
@@ -21,36 +62,27 @@ function Cart() {
                         <img className='pedido' src={pedido} alt='NOT FOUND' />
                     </div>
                     <div className='centerCart'>
-                        <div className='topCenterCart'>
+                        {products.map((product, index) => //pasar producto e índice
+                            <div className='topCenterCart' key={product.id_product}>
+                                <div>
+                                    <p className='menuTitle'>{product.name}</p>
+                                </div>
 
-                            <p className='menuTitle'>Menú McDowell's Jr</p>
+                                <div className='topCenterCartDiv'>
+                                    <img className='mcJrCart' src={product.image} alt='NOT FOUND' />
+                                    <button className='menos'>-</button>
+                                    <div className='cantidad'>{context.cart[index].quantity}</div>
+                                    <button className='mas' onClick={() => addProduct(product.id_product)}>+</button>
+                                    <div className='precioUd'>{product.price}€</div>
+                                    <div className='precioTotMen'>{product.price * context.cart[0].quantity}€</div>
+                                </div>
 
-                            <div className='topCenterCartDiv'>
-                                <img className='mcJrCart' src='https://cdn-icons-png.flaticon.com/512/7451/7451012.png' alt='NOT FOUND' />
-                                <button className='menos'>-</button>
-                                <div className='cantidad'></div>
-                                <button className='mas'>+</button>
-                                <div className='precioUd'></div>
-                                <div className='precioTotMen'></div>
-                            </div>
-                        </div>
-                        <div className='bottomCenterCart'>
+                            </div>)}
 
-                            <p className='menuTitle'>Menú McDowell's</p>
-
-                            <div className='bottomCenterCartDiv'>
-                                <img className='mcCart' src='https://cdn-icons-png.flaticon.com/512/9425/9425772.png' alt='NOT FOUND' />
-                                <button className='menos'>-</button>
-                                <div className='cantidad'></div>
-                                <button className='mas'>+</button>
-                                <div className='precioUd'></div>
-                                <div className='precioTotMen'></div>
-                            </div>
-                        </div>
-                        <p className='totalCart'> TOTAL: { } € </p>
+                        <p className='totalCart'> TOTAL: {context.totalCart[0].totalPrice}€ </p>
                     </div>
                     <div className='bottomCart'>
-                        <button className='resumeCart' onClick={ ()=>navigate(`/menus`)}>
+                        <button className='resumeCart' onClick={() => navigate(`/menus`)}>
                             REANUDAR PEDIDO
                         </button>
                         <button className='finishCart'>
--- mcdowells-proyect/src/components/clients/ProductDetails.jsx
+++ mcdowells-proyect/src/components/clients/ProductDetails.jsx
@@ -5,24 +5,68 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import axios from 'axios';
 import { useState } from 'react';
+import { useCartContext } from '../../context/ShoppingCartContext';
 
 
 function ProductDetails() {
 
     const navigate = useNavigate();
-    const [menu, setMenu] = useState([]);
-    const { id_product } = useParams();
-    const server = `http://localhost:3001/api/menus/${id_product}`;
+    const [product, setProduct] = useState([]);
+    const { id } = useParams();
+    const context = useCartContext();
 
 
     useEffect(() => {
-        const getMenu = async () => {
-            const response = await axios.get(server);
-            setMenu(response.data);
+        const getProduct = async () => {
+            const response = await axios.get(`http://localhost:3001/api/products/${id}`);
+            setProduct(response.data);
             console.log(response.data);
         }
-        getMenu();
+        getProduct();
     }, [])
+
+    const addProduct = () => {
+
+        //Comprobamos si existe ya en el carrito un producto igual al añadido
+        const isInCart = context.cart.find(item => item.id_product === product.id_product)
+
+        /*Si está en el carrito lo que hacemos es recorrerlo y sumamos 1, es decir,  
+        todos aquellos cuyo id no coindida, quiero que se mantengan tal cual pero si
+        hay algún producto en el carrito que sí coincide, lo que quiero es que me 
+        modifique la cantidad, me lo dejas tal cual con ...product pero le 
+        añadimos 1 con product.quantity +1 */
+
+        if (isInCart) {
+            const setOneProd = context.cart.map(item =>  //En este punto, si coincide isInCart es lo mismo que product
+                item.id_product === isInCart.id_product ? {
+                    ...item, quantity: item.quantity + 1, total: product.price * (item.quantity + 1)
+                } : item
+            );
+            context.setCart(setOneProd); //Estás limpiando el carrito que había previamente se actualiza con lo nuvo
+            console.log(setOneProd);
+        } else {
+            /*Si no está lo que hacemos es otro array que le indicamos que tiene que meter todo
+             lo que había previamente en el carrito con ...cart para después añadirle el nuevo 
+             producto y la cantidad de este, que es 1 */
+            context.cart.push({
+                ...product,
+                total: product.price,
+                quantity: 1
+
+            })
+        }
+
+        const setTotalPrice = context.totalCart.map((item) => {
+            return (
+                {
+                    totalPrice: item.totalPrice + product.price,
+                    totalQuantity: item.totalQuantity+product.quantity
+                })
+        })
+        context.setTotalCart(setTotalPrice)//actualizamos el total
+
+    }
+
 
     return (
         <>
@@ -32,23 +76,27 @@ function ProductDetails() {
                     <div className='topDetails'>
                         <img className='detailsTitle' src={details} alt='NOT FOUND' />
                     </div>
-                    {menu.map((one) =>
-                        <div className='detailsContainer' key={one.id_product}>
-                        <p className='menu'>{one.name}</p>
+                    <div className='detailsContainer' key={product.id_product}>
+                        <p className='menu'>{product.name}</p>
 
-                            <div className='centerDetails'>
-                                <img className='detailsMc' src={one.image} alt='NOT FOUND' />
-                                <p className='pDetails'>
-                                    {one.description}
-                                </p>
-                            </div>
-                            <p className='menuPrice'>Precio: {one.price}€</p>
-
+                        <div className='centerDetails'>
+                            <img className='detailsMc' src={product.image} alt='NOT FOUND' />
+                            <p className='pDetails'>
+                                {product.description}
+                            </p>
                         </div>
-                    )}
+                        <p className='menuPrice'>
+                            Precio: {product.price}€
+                        </p>
+                        <p className='menuPrice'>
+                            Total carrito: {context.totalCart[0].totalPrice}€
+                        </p>
+
+                    </div>
+
                     <div className='bottomMenu'>
                         <div className='bottomLeftDetails' >
-                            <button className='add'>AÑADIR AL PEDIDO</button>
+                            <button className='add' onClick={() => addProduct(product.id_product)}>AÑADIR AL PEDIDO</button>
                         </div>
                         <div className='bottomRightDetails'>
                             <button className='select ' onClick={() => navigate(`/menus`)}>VOLVER A SELECCIÓN</button>
@@ -74,5 +122,5 @@ export default ProductDetails;
                                 Con queso parmesano y queso americano ligeramente ahumado, bacon crispy, lechuga batavia, pomodoro y cebolla morada.
                                 <br />
                                 Todo ello acompañado de nuestras patatas bastón ecológicas cortadas en el día y cocinadas en aceite de oliva.
-
+ 
 */
