b3c0d389f72ea35e5f7dd02d181df7cbd502f588
TanUve <tania.velasco.carvajal@gmail.com>
2023-02-05 00:18:13 +0100

    new component and functionality

---
+++ mcdowells-proyect/src/components/clients/LastPage.jsx
@@ -0,0 +1,36 @@
+import '../../assets/clients/clientshome.css'
+import pronto from '../../assets/images/pronto.png'
+import mcTitle from '../../assets/images/title.png'
+import vuelva from '../../assets/images/vuelva.png'
+import React from 'react';
+
+
+function LastPage() {
+
+    function redireccionar() {
+        setTimeout("location.href='http://localhost:3000/'", 5000)
+        //Falta poner que cuando esto pase el context se reinicie y no quede nada.;
+      }
+   
+    return (
+        <>
+            <div className="container" onLoad={()=>redireccionar()}>
+                <div className='leftContainer'></div>
+                <div className="centerContainer">
+                    <div className='topDiv'>
+                        <img className='mcTitleLast' src={mcTitle} alt='NOT FOUND' />
+                    </div>
+                    <img className='vuelva' src={vuelva} alt='NOT FOUND' />
+                    <img className='pronto' src={pronto} alt='NOT FOUND' />
+
+                    <div className='bottomDiv'>
+                    </div>
+                </div>
+                <div className='rigthContainer'>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LastPage;
