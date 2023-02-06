d7e9c731a4d67f0ea8ef97ebf50801cb29dc1e52
PauloBazan <paulobazan155@gmail.com>
2023-02-06 20:36:25 +0100

    add email to backend

--- mcdowells-proyect/src/components/clients/EmailPage.jsx
+++ mcdowells-proyect/src/components/clients/EmailPage.jsx
@@ -2,7 +2,8 @@ import email from '../../assets/images/email.png'
 import '../../assets/clients/emailpage.css'
 import { useNavigate } from 'react-router-dom';
 import { useCartContext } from '../../context/ShoppingCartContext';
-
+import { useState } from 'react';
+import axios from 'axios'
 
 
 
@@ -11,7 +12,16 @@ function EmailPage() {
     const context = useCartContext();
     const image = 'https://cdn-icons-png.flaticon.com/512/1053/1053188.png?w=740&t=st=1675464717~exp=1675465317~hmac=123970fb6328e4fa8a9eb22784499c906aaddd8cf173382cf6cca075051fc494';
 
-   
+    const [userEmail, setUserEmail] = useState()
+    const sendCart = async(e) => {
+        e.preventDefault() 
+        let order = await context.cart
+        await order.push({ ...context.totalCart[0], email: userEmail})
+       //const response = await axios.post("http://localhost:3001/api/orders/create-order", order)
+       context.cart
+        console.log(order)
+        navigate('/seeyousoon')
+    }  
 
     return (
         <>
@@ -23,13 +33,14 @@ function EmailPage() {
                         <img className='emailImg' src={email} alt='NOT FOUND' />
                     </div>
                     <div className='centerEmail' >
-                        <form className='centerEmail'>
+                        <form className='centerEmail' onSubmit={sendCart} >
                             <label className='infoText' for="fname">
                                 A continuación introduzca un email válido para remitirle el ticket correspondiente a su pedido.
 
                             </label>
-                            <input className='email' type="text" id="emailOrder" name="clientemail" placeholder="Introduzca email" required>
-                            </input>
+                            <input className='email' type="text" id="emailOrder" name="clientemail" placeholder="Introduzca email" onChange={(e) => setUserEmail(e.target.value)}
+                                required />
+
                             <input className='sendEmail' type="submit" value="ENVIAR" />
 
 
@@ -39,7 +50,7 @@ function EmailPage() {
                     <div className='bottomEmail'>
 
                         <p className='totalEmail'>
-                            <img className='returnBtnEmail' src={image} onClick={()=> navigate('/menus')}/>
+                            <img className='returnBtnEmail' src={image} onClick={() => navigate('/menus')} />
 
                             TOTAL: {context.totalCart[0].totalPrice}€
                         </p>
--- mcdowells-proyect/src/components/clients/LastPage.jsx
+++ mcdowells-proyect/src/components/clients/LastPage.jsx
@@ -3,15 +3,15 @@ import pronto from '../../assets/images/pronto.png'
 import mcTitle from '../../assets/images/title.png'
 import vuelva from '../../assets/images/vuelva.png'
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 function LastPage() {
 
+    const navigate= useNavigate()
     function redireccionar() {
-        // Esta función lo que hace es que redirecciona a la home cuando pasa el tiempo establecido
-        // En este caso falta poner que se vacíe el conetxto cuando pase el tiempo, de tal forma que se
-        // pueda iniciar un nuevo pedido desde createContext.
-        setTimeout("location.href='http://localhost:3000/'", 5000)
+        
+        setTimeout(()=>navigate('/'), 5000)
         //Falta poner que cuando esto pase el context se reinicie y no quede nada.;
       }
    
