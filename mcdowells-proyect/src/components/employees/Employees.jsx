bcd0dd9a46d1c2285e4674fccaaaeafa6037d108
TanUve <tania.velasco.carvajal@gmail.com>
2023-02-18 00:01:20 +0100

    update employees css

--- mcdowells-proyect/src/components/employees/Employees.jsx
+++ mcdowells-proyect/src/components/employees/Employees.jsx
@@ -5,7 +5,7 @@ import '../../assets/employees/employees.css';
 
 function Employees() {
   return (
-    <div className='container-enployees'>
+    <div className='container-employees'>
       <div className='container-navbar'>
         <Navbar/>
       </div>
