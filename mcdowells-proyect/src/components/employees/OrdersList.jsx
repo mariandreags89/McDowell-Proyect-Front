abdafb623bbd58de0e78fb23b3f762ee4522e11f
gema <gjcuenca@hotmail.com>
2023-02-22 12:25:20 +0100

    detelorders

--- mcdowells-proyect/src/components/employees/OrdersList.jsx
+++ mcdowells-proyect/src/components/employees/OrdersList.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import OrdersCard from "./OrdersCard";
 import '../../assets/employees/ordersList.css';
 
 const OrdersList = ({statu}) =>{
-  console.log(statu)
+  
+    const [ordersDetail, setOrdersDetail] = useState([]);
+
+    useEffect(() => {
+        const getOrdersDetail = async () => {
+            const response = await axios.get(`http://localhost:3001/api/orders/ordersDetail/${statu.id_status}`);
+            setOrdersDetail(response.data);
+            
+        }
+        getOrdersDetail();
+    }, [])
+
+    
   return (
         <div className="container-card">
             <div className='title-order'>
               <h1 className='title'>{statu.description}</h1>
             </div>
-            <OrdersCard/>
-            <OrdersCard/>
+            {ordersDetail.map((order) =>(
+              <OrdersCard order={order}/>
+              ))}
         </div>
   )
 }
