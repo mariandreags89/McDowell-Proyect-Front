327fd955fded8217e0eda9ffef42d11fe7332eb7
gema <gjcuenca@hotmail.com>
2023-02-24 19:13:48 +0100

    union

--- mcdowells-proyect/src/components/employees/OrdersList.jsx
+++ mcdowells-proyect/src/components/employees/OrdersList.jsx
@@ -6,6 +6,7 @@ import '../../assets/employees/ordersList.css';
 const OrdersList = ({statu}) =>{
   
     const [ordersDetail, setOrdersDetail] = useState([]);
+    const [filtered, setFiltered]= useState([]);
 
     useEffect(() => {
         const getOrdersDetail = async () => {
@@ -14,8 +15,16 @@ const OrdersList = ({statu}) =>{
             
         }
         getOrdersDetail();
-    }, [])
+    }, [statu])
 
+    useEffect(() => {
+      let numbers=[];
+      for (let i = 0; i < ordersDetail.length; i++) {
+        numbers.push(ordersDetail[i].id_order)
+      };
+      setFiltered([...new Set(numbers)]);
+    }, [ordersDetail]);
+  
     
   return (
         <div className="container-card">
@@ -23,10 +32,8 @@ const OrdersList = ({statu}) =>{
               <p className='title'>{statu.description}</p>
             </div>
             <div className='statusDiv'>
-            {ordersDetail.map((order) =>(
-              <OrdersCard order={order}/>
-              ))}
-              </div>
+              <OrdersCard ordersDetail={ordersDetail} filtered={filtered}/>
+            </div>
         </div>
   )
 }
