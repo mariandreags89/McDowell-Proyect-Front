de7d4baee9f7d004cdc760745370ffbef0bd10ba
PauloBazan <paulobazan155@gmail.com>
2023-02-25 10:40:03 +0100

    fix errors

--- mcdowells-proyect/src/components/employees/Navbar.jsx
+++ mcdowells-proyect/src/components/employees/Navbar.jsx
@@ -7,7 +7,7 @@ import { useUserContext } from '../../context/User';
 const Navbar = () => {
   const navigate = useNavigate()
   const contextUser = useUserContext()
-  const username = (contextUser.user.usermane).toUpperCase()
+  //const username = contextUser.user.username
 
   const logOut = () => {
     contextUser.setUser([])
@@ -19,10 +19,10 @@ const Navbar = () => {
       <div className='leftNav'>
         <img className='logo-img' alt='NOT FOUND' src={logo} />
         <img className='emplImg' src='https://cdn-icons-png.flaticon.com/512/3899/3899618.png' alt='NOT FOUND' />
-        {username}
+        {contextUser.user.username.toUpperCase()}
       </div>
       <div className='rightNav' >
-        <img className='logOutImg' src='https://cdn-icons-png.flaticon.com/512/8771/8771121.png' alt='NOT FOUND'  />
+        <img className='logOutImg' src='https://cdn-icons-png.flaticon.com/512/8771/8771121.png' alt='NOT FOUND' />
         <label className='logOut' onClick={() => logOut()}>LOG OUT</label>
       </div>
     </div>
