507725b79995b62e6f5497c0431c1c55cf929658
TanUve <tania.velasco.carvajal@gmail.com>
2023-02-25 09:58:16 +0100

    add employee username and modify navbar css

--- mcdowells-proyect/src/components/employees/Navbar.jsx
+++ mcdowells-proyect/src/components/employees/Navbar.jsx
@@ -10,17 +10,7 @@ const Navbar = () => {
 
   const navigate = useNavigate();
   const contextUser = useUserContext();
-  const [employee, setEmployee] = useState([]);
-  const id_user = contextUser.user.id_user;
-
-  useEffect(() => {
-
-    const getUser = async () => {
-      const response = await axios.get(`http://localhost:3001/api/worker/${id_user}`);
-      setEmployee((response.data[0].username).toUpperCase());
-    }
-    getUser();
-  }, [id_user])
+  const username= (contextUser.user.username).toUpperCase();
 
 
   const logOut = () => {
@@ -34,7 +24,7 @@ const Navbar = () => {
       <div className='leftNav'>
         <img className='logo-img' alt='NOT FOUND' src={logo} />
         <img className='emplImg' src='https://cdn-icons-png.flaticon.com/512/3899/3899618.png' alt='NOT FOUND' />
-        {employee}
+        {username}
       </div>
       <div className='rightNav'>
         <img className='logOutImg' src='https://cdn-icons-png.flaticon.com/512/8771/8771121.png' alt='NOT FOUND' onClick={() => logOut()} />
