e1a34284bdbbdbf046449347c7b10b45fb902eef
TanUve <tania.velasco.carvajal@gmail.com>
2023-02-25 10:40:08 +0100

    fix errors

--- mcdowells-proyect/src/components/employees/Navbar.jsx
+++ mcdowells-proyect/src/components/employees/Navbar.jsx
@@ -3,14 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import '../../assets/employees/navbar.css';
 import logo from '../../assets/images/logo.ico';
 import { useUserContext } from "../../context/User";
-import axios from 'axios';
 
 
 const Navbar = () => {
 
   const navigate = useNavigate();
   const contextUser = useUserContext();
-  const username= (contextUser.user.username).toUpperCase();
 
 
   const logOut = () => {
@@ -24,7 +22,7 @@ const Navbar = () => {
       <div className='leftNav'>
         <img className='logo-img' alt='NOT FOUND' src={logo} />
         <img className='emplImg' src='https://cdn-icons-png.flaticon.com/512/3899/3899618.png' alt='NOT FOUND' />
-        {username}
+        {(contextUser.user.username).toUpperCase()}
       </div>
       <div className='rightNav'>
         <img className='logOutImg' src='https://cdn-icons-png.flaticon.com/512/8771/8771121.png' alt='NOT FOUND' onClick={() => logOut()} />
