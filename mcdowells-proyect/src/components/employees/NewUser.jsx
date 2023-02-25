5caa3f56b621d7acb828d6294e35e1484b3c8322
TanUve <tania.velasco.carvajal@gmail.com>
2023-02-25 14:01:42 +0100

    update orderCard and newUser components

--- mcdowells-proyect/src/components/employees/NewUser.jsx
+++ mcdowells-proyect/src/components/employees/NewUser.jsx
@@ -1,13 +1,19 @@
 import '../../assets/employees/newUser.css';
+import { useUserContext } from '../../context/User';
 
 
 
 const NewUser = () => {
 
+    const contexUser = useUserContext()
 
-  return (
-   <button className='addNewEmplo'>AÑADIR USUARIO</button>
-  )
+
+    return (
+        <>
+            {contexUser.user.role !== "admin" ? <></> :
+                <button className='addNewEmplo'>AÑADIR USUARIO</button>}
+        </>
+    )
 }
 
 export default NewUser
--- mcdowells-proyect/src/components/employees/OrdersCard.jsx
+++ mcdowells-proyect/src/components/employees/OrdersCard.jsx
@@ -9,14 +9,15 @@ const OrdersCard = ({ ordersDetail, filtered }) => {
   // const [status,setStatus]=useState();
   // const [number,setNumber]=useState();
   const contexUser = useUserContext()
+
   let status = '';
 
   const nextStatus = async (one) => {
-    
+
     const token = contexUser.user.token
     const authAxios = axios.create({
-      headers : {
-        authorization : token
+      headers: {
+        authorization: token
       }
     })
 
@@ -40,10 +41,15 @@ const OrdersCard = ({ ordersDetail, filtered }) => {
                   <p className='none'>{status = order.id_status}</p>
                   <p className="orderName"> •{order.name}</p>
                   <p className="productQ">Cantidad: {order.units}</p>
+                  {(contexUser.user.role === "admin" && order.id_status !== 5) ||
+                    (contexUser.user.role === "waiter" && order.id_status !== 5) ||
+                    (contexUser.user.role === "chef" && order.id_status !== 3) ?
+                    <button className='next' onClick={() => nextStatus(one)}>NEXT STATUS</button> : <></>}
                 </>
+
               )}
           </div>
-          <button className='next' onClick={() => nextStatus(one)}>NEXT STATUS</button>
+
           <p className='divisor'>--------------------</p>
         </div>
       ))
