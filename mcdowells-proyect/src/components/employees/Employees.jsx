34a88c1dcafc81ffe44dbb37365ccdc16d3136f0
TanUve <tania.velasco.carvajal@gmail.com>
2023-02-25 13:01:44 +0100

    add new button component

--- mcdowells-proyect/src/components/employees/Employees.jsx
+++ mcdowells-proyect/src/components/employees/Employees.jsx
@@ -5,43 +5,50 @@ import '../../assets/employees/employees.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useUserContext } from "../../context/User";
- 
+import NewUser from './NewUser';
+
 function Employees() {
-  
-    const contextUser = useUserContext();
-    const [status, setStatus] = useState([]);
-    const [access, setAccess] = useState([]);
-    const id_user=contextUser.user.id_user
 
-    useEffect(() => {
+  const contextUser = useUserContext();
+  const [status, setStatus] = useState([]);
+  const [access, setAccess] = useState([]);
+  const id_user = contextUser.user.id_user
 
-      const getAccess = async () => {
-          const response = await axios.get(`http://localhost:3001/api/worker/${id_user}`);
-          setAccess(response.data[0].state);
-        
-      }
-      getAccess();
-      
-      
+  useEffect(() => {
+
+    const getAccess = async () => {
+      const response = await axios.get(`http://localhost:3001/api/worker/${id_user}`);
+      setAccess(response.data[0].state);
+
+    }
+    getAccess();
+
+
   }, [id_user])
-     
 
-    useEffect(() => {
-      
-        const getStatus = async () => {
-            // const response = await axios.get(`http://localhost:3001/api/status/${in_status}`);
-            const response = await axios.get(`http://localhost:3001/api/status/${access}`);
-            setStatus(response.data);
-            
-        }
-        getStatus();
-    }, [access])
+
+  useEffect(() => {
+
+    const getStatus = async () => {
+      // const response = await axios.get(`http://localhost:3001/api/status/${in_status}`);
+      const response = await axios.get(`http://localhost:3001/api/status/${access}`);
+      setStatus(response.data);
+
+    }
+    getStatus();
+  }, [access])
 
   return (
     <div className='container-employees'>
-        <div  className="navEmpl"><Navbar/> </div>
-      {status.map((statu) =>(
-        <OrdersList statu={statu}/>
+      <div className="navEmpl">
+        <Navbar />
+      </div>
+      <div className='btnAddNew'>
+        <NewUser/>
+      </div>
+      <br/>
+      {status.map((statu) => (
+        <OrdersList statu={statu} />
       ))}
 
     </div>
---
+++ mcdowells-proyect/src/components/employees/NewUser.jsx
@@ -0,0 +1,13 @@
+import '../../assets/employees/newUser.css';
+
+
+
+const NewUser = () => {
+
+
+  return (
+   <button className='addNewEmplo'>AÑADIR USUARIO</button>
+  )
+}
+
+export default NewUser
